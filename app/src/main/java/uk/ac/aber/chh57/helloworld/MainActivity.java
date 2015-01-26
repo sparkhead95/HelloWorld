bbad8e71c04f6f4f24483dbeca4a8c4b521cf798
sparkhead95 <christianosholland@gmail.com>
2015-01-26 11:34:55 +0000

    Camera stuff added along with comments

--- app/src/main/java/uk/ac/aber/chh57/helloworld/MainActivity.java
+++ app/src/main/java/uk/ac/aber/chh57/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -21,15 +22,25 @@ import android.location.Location;
 import android.view.View.OnClickListener;
 import android.location.LocationListener;
 import android.location.LocationManager;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
 
 
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity  {
+
+    File plantFile,sceneFile;
     protected LocationManager locationManager;
     protected Button GPSBtn;
+    protected Button plantPhotoBtn,scenePhotoBtn;
     private static final long update_distance = 5; //the distance change before an update(in meters)
     private static final long update_time = 1000 ; //time between updates (in milliseconds)
     public static String userData = "sharedData";
@@ -41,19 +52,24 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // *********************** pre-tab creation *******************************************************
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         runtimeData = getSharedPreferences(userData, 0);
-        checkPrefs();
+        // The below function call finds the user details from the shared preferences, if they exist
+        //checkPrefs();
 
-        // The class doesn't check through tabs to find a value; it checks for ID matches.
+        // The below assigns the fields in the gradle to the program code
         txtName = (EditText) findViewById(R.id.nametxt);
         txtPhone = (EditText) findViewById(R.id.phonetxt);
         txtEmail = (EditText) findViewById(R.id.emailtxt);
+        // and assigns the tab host to control which tabs exist
         TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
-
+        // it then initiates the tabs
         tabHost.setup();
+        // ************************************************************************************************
 
+        // *************************** the below creates the three tabs *****************************************
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("usrSettings");
         tabSpec.setContent(R.id.Settings);
         tabSpec.setIndicator("Settings");
@@ -68,7 +84,10 @@ public class MainActivity extends Activity {
         tabSpec.setContent(R.id.Records);
         tabSpec.setIndicator("List of Records");
         tabHost.addTab(tabSpec);
+        // *******************************************************************************************************
 
+
+        // *************************** GPS METHOD ***************************************************************
         GPSBtn = (Button) findViewById(R.id.btnSave);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -86,7 +105,10 @@ public class MainActivity extends Activity {
                 showCurrentLocation();
             }
         });
+        // *************************************************************************************************
 
+
+        // **************************************************************************************************
         // The following is for actions based on text changing within the WHOLE app- this main class doesn't see tabs; it only views the ID's.
         txtName.addTextChangedListener(new TextWatcher() {
             @Override
@@ -104,7 +126,9 @@ public class MainActivity extends Activity {
 
             }
         });
+        // **************************************************************************************************
 
+        // ***************************** Saving user settings *********************************************
         saveBtn = (Button) findViewById(R.id.btnSaveSettings);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -121,7 +145,67 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "Details for " + txtName.getText().toString() + " have been saved.", Toast.LENGTH_SHORT).show();
             }
         });
+        // ******************************************************************************************************
+
+        // ******************* Photo storage button control **************************************************
+        plantPhotoBtn = (Button) findViewById(R.id.btnPlant);
+        scenePhotoBtn = (Button) findViewById(R.id.btnScene);
+
+        plantPhotoBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Get the name from the setPhotoFile() function
+                plantFile = setPhotoFile(0);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, plantFile);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+        scenePhotoBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Get the name from the setPhotoFile() function
+                sceneFile = setPhotoFile(1);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, sceneFile);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+        // ******************************************************************************************************
     }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+            // Image captured and saved successfully
+                Toast.makeText(this, "Image saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+            // User cancelled the photo
+                Toast.makeText(this, "Image not saved", Toast.LENGTH_LONG).show();
+            } else {
+            // Photo failed, inform user
+                Toast.makeText(this, "Image failed to saved", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File setPhotoFile(int x){
+        String type = null;
+        if (x==0){
+        //if it's plant
+            type = "Species";
+        }else if (x==1){
+        //if it's scene
+            type = "Scene";
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File( "IMG_" + type + timeStamp + ".jpg");//directory.getPath() + File.separator +
+    }
+
 
     protected void showCurrentLocation(){
 
@@ -143,9 +227,14 @@ public class MainActivity extends Activity {
 
         }
         else {
-            txtName.setText(runtimeData.getString("username", "Not Found"));
-            txtPhone.setText(runtimeData.getString("phoneNumber", "Not Found"));
-            txtEmail.setText(runtimeData.getString("email", "Not Found"));
+            if (runtimeData.getString("username", "Not Found") != null){
+                txtName.setText(runtimeData.getString("username", "Not Found"));
+                txtPhone.setText(runtimeData.getString("phoneNumber", "Not Found"));
+                txtEmail.setText(runtimeData.getString("email", "Not Found"));
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "This sharedpreference is empty.", Toast.LENGTH_SHORT).show();
+            }
         }
 
 
